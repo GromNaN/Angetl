267f75aa5558ef80a7ad67f7407fb09b15f74cce
Jérôme Tamarelle <jerome@tamarelle.net>
2011-10-05 23:45:43 +0200

    [Reader] Create XMLReader

---
+++ src/Angetl/AbstractReader.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Angetl;
+
+abstract class AbstractReader
+{
+    protected $fieldNames;
+    protected $currentRecord = null;
+    protected $opened = false;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->fieldNames = array();
+    }
+    
+    /**
+     * Open reader
+     *
+     * @throw \LogicException When already opened
+     * @api
+     */
+    public function open()
+    {
+        $this->_assertNotOpened();
+        $this->_open();
+        $this->opened = true;
+
+        return $this;
+    }
+
+    /**
+     * Close reader
+     *
+     * @throw \LogicException When not opened
+     * @api
+     */
+    public function close()
+    {
+        $this->_assertOpened();
+        $this->_close();
+        $this->opened = false;
+
+        return $this;
+    }
+
+    /**
+     * Read next record
+     *
+     * @return 
+     * @throw \LogicException When not opened
+     * @api
+     */
+    public function read()
+    {
+        $this->_assertOpened();
+        $this->_newRecord();
+
+        if ($this->_read()) {
+            return $this->currentRecord;
+        } else {
+            return null;
+        }
+    }
+
+    abstract protected function _read();
+
+    abstract protected function _close();
+
+    abstract protected function _open();
+
+    /**
+     * Create a new current record.
+     */
+    protected function _newRecord()
+    {
+        $this->currentRecord = array_fill_keys($this->fieldNames, null);
+    }
+
+    /**
+     * @return array Current record
+     */
+    public function getCurrentRecord()
+    {
+        return $this->currentRecord;
+    }
+
+    /**
+     * @return array Field names
+     */
+    public function getFieldNames()
+    {
+        return $this->fieldNames;
+    }
+
+    /**
+     * @throw \LogicException
+     */
+    protected function _assertOpened()
+    {
+        if (!$this->opened) {
+            throw new \LogicException('Reader not opened.');
+        }
+    }
+
+    /**
+     * @throw \LogicException
+     */
+    protected function _assertNotOpened()
+    {
+        if ($this->opened) {
+            throw new \LogicException('Reader already opened.');
+        }
+    }
+}
---
+++ tests/Angetl/Tests/Reader/XmlReaderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Angetl\Tests\Reader;
+
+use Angetl\Reader\XmlReader;
+
+class XmlReaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRead()
+    {
+        $reader = $this->getXmlReader();
+
+        $expected = array(
+            0 => array(
+                'title' => 'Harry Potter',
+                'language' => 'eng',
+                'price' => '29.99',
+            ),
+            1 => array(
+                'title' => 'Leçons de XML',
+                'language' => 'fra',
+                'price' => '39.95',
+            ),
+        );
+
+        $reader->open();
+
+        foreach ($expected as $i => $expect) {
+            $record = $reader->read();
+            $this->assertEquals($expect, $record, 'Record '.$i.' is read');
+        }
+
+        $this->assertNull($reader->read(), 'End of file');
+        
+        $reader->close();
+    }
+
+    public function testAddField()
+    {
+        $reader = $this->getXmlReader();
+
+        $fieldNames = array('title', 'language', 'price');
+
+        $this->assertEquals($fieldNames, $reader->getFieldNames(), 'Get field names');
+    }
+
+    protected function getXmlReader()
+    {
+        $reader = new XmlReader(__DIR__.'/Fixtures/bookstore.xml');
+        $reader
+          ->setRecordXpath('//book')
+          ->addField('title', 'string(./title/text())')
+          ->addField('language', 'string(./title/@lang)')
+          ->addField('price', 'string(./price/text())')
+        ;
+
+        return $reader;
+    }
+}
